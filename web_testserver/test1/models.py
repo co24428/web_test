df111b8216fc87768f09dda185600dd6bfa7fe59
ihwan <ihwan1236@gmail.com>
2020-01-17 18:05:30 +0900

    FK까지 모델작업

--- web_testserver/test1/models.py
+++ web_testserver/test1/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+
+class cctv_data(models.Model):
+    objects = models.Manager()
+
+    no          = models.AutoField(primary_key=True)
+    big_addr    = models.CharField(max_length=30)
+    small_addr  = models.CharField(max_length=30)
+    cctv_yn     = models.CharField(max_length=5)
+    cctv_num    = models.IntegerField()
+
+class user_table(models.Model):
+    objects = models.Manager()
+
+    user_id     = models.CharField(max_length=30, primary_key=True)
+    name        = models.CharField(max_length=20)
+    password    = models.CharField(max_length=30)
+    age         = models.IntegerField()
+    home        = models.CharField(max_length=70)
+
+class article(models.Model):
+    objects = models.Manager()
+
+    no          = models.AutoField(primary_key=True)
+    title       = models.CharField(max_length=70)
+    thumbnail   = models.BinaryField(null=True)
+    report      = models.CharField(max_length=20)
+    content     = models.TextField()
+    pub_date     = models.DateField()
+
+class favorite(models.Model):
+    objects = models.Manager()
+
+    no          = models.AutoField(primary_key=True)
+    user_id     = models.ForeignKey(user_table, on_delete = models.CASCADE)
+    region_no   = models.ForeignKey(cctv_data, on_delete = models.CASCADE)
+
+class article_scrap(models.Model):
+    objects = models.Manager()
+
+    no          = models.AutoField(primary_key=True)
+    user_id     = models.ForeignKey(user_table, on_delete = models.CASCADE)
+    article_no   = models.ForeignKey(article, on_delete = models.CASCADE)
+    scrap_date  = models.DateField()
--- web_testserver/web_testserver/settings.py
+++ web_testserver/web_testserver/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'test1',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'web_testserver.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +76,16 @@ WSGI_APPLICATION = 'web_testserver.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        # oracle
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'xe', #SID
+        'USER': 'admin1',
+        'PASSWORD': '1234',
+        'HOST': '192.168.99.100',
+        'PORT': '32764'
     }
 }
 
@@ -118,3 +127,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
